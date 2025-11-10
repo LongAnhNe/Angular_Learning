a4b27bb736f77a3c541ab7219df04d9ca7a015eb
Longanhne <longnh11092000@gmail.com>
2025-11-10 12:00:08 +0700

    update routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { UserListComponent } from './user-list/user-list.component';
+import { HeaderLayoutComponent } from './share/header-layout/header-layout.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone : true,
+  imports: [HeaderLayoutComponent, UserListComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'],
+  standalone: true
+})
+export class UserListComponent {
+  users = [
+    { id: 1, name: 'Nguyễn Văn A', email: 'a@example.com' },
+    { id: 2, name: 'Trần Thị B', email: 'b@example.com' }
+  ];
+  
+  selectedUser: any = null;
+  
+  selectUser(user: any) {
+    this.selectedUser = user;
+  }
+}
